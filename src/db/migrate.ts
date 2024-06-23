467284110094085d8fe35103905bd9395db6d01d
Rafael Freitas <raffreitasx@outlook.com>
2024-06-23 16:54:18 -0300

    feat: add eslint and lint project

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,18 +1,17 @@
-import postgres from "postgres";
+import postgres from 'postgres'
 import chalk from 'chalk'
 
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 
-import { env } from "../env";
+import { env } from '../env'
 
 const connection = postgres(env.DATABASE_URL, { max: 1 })
 const db = drizzle(connection)
 
-
 await migrate(db, { migrationsFolder: 'drizzle' })
 
-console.log(chalk.greenBright('Migrations applied successfully!'));
+console.log(chalk.greenBright('Migrations applied successfully!'))
 
 await connection.end()
 
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,3 +1,5 @@
+/* eslint-disable drizzle/enforce-delete-with-where */
+
 import { faker } from '@faker-js/faker'
 import { restaurants, users } from './schema'
 import { db } from './connection'
@@ -6,62 +8,60 @@ import chalk from 'chalk'
 /**
  * Reset database
  */
-
 await db.delete(users)
 await db.delete(restaurants)
 
-console.log(chalk.yellow('✅ Database reset!'));
+console.log(chalk.yellow('✅ Database reset!'))
 
 /**
  * Create customers
  */
-
 await db.insert(users).values([
   {
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    role: 'customer'
+    role: 'customer',
   },
   {
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    role: 'customer'
+    role: 'customer',
   },
 ])
 
-console.log(chalk.yellow('✅ Created customers!'));
+console.log(chalk.yellow('✅ Created customers!'))
 
 /**
  * Create manager
  */
+const [manager] = await db
+  .insert(users)
+  .values([
+    {
+      name: faker.person.fullName(),
+      email: 'admin@admin.com',
+      role: 'manager',
+    },
+  ])
+  .returning({
+    id: users.id,
+  })
 
-const [manager] = await db.insert(users).values([
-  {
-    name: faker.person.fullName(),
-    email: 'admin@admin.com',
-    role: 'manager'
-  },
-]).returning({
-  id: users.id
-})
-
-console.log(chalk.yellow('✅ Created manager!'));
+console.log(chalk.yellow('✅ Created manager!'))
 
 /**
  * Create restaurant
  */
-
 await db.insert(restaurants).values([
   {
     name: faker.company.name(),
     description: faker.lorem.paragraph(),
-    managerId: manager.id
+    managerId: manager.id,
   },
 ])
 
-console.log(chalk.yellow('✅ Created restaurant!'));
+console.log(chalk.yellow('✅ Created restaurant!'))
 
-console.log(chalk.greenBright('Database seeded successfully!'));
+console.log(chalk.greenBright('Database seeded successfully!'))
 
 process.exit()
-
