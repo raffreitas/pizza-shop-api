a2c3d4694af7a40d0d924a9d3d270faf5420df31
Rafael Freitas <raffreitasx@outlook.com>
2024-06-23 17:25:52 -0300

    feat: add swagger and routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,12 @@
 import { Elysia } from 'elysia'
+import { registerRestaurant } from './routes/register-restaurant'
+import { sendAuthLink } from './routes/send-auth-link'
+import swagger from '@elysiajs/swagger'
 
-const app = new Elysia().get('/', () => {
-  return 'Hello World'
-})
+const app = new Elysia()
+  .use(swagger())
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('HTTP server running!')
