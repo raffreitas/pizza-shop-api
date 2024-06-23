4973154b5857444877dd16c67f50c0069a9dd856
Rafael Freitas <raffreitasx@outlook.com>
2024-06-23 16:25:12 -0300

    feat: add editor config and lint

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,10 @@
 import { Elysia } from 'elysia'
 
 const app = new Elysia()
-    .get('/', () => {
-        return 'Hello World'
-    })
+  .get('/', () => {
+    return 'Hello World'
+  })
 
 app.listen(3333, () => {
-    console.log('HTTP server running!')
+  console.log('HTTP server running!')
 })
