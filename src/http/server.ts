6eb86a432946fe781a9dde231bd239030daf7be1
Rafael Freitas <raffreitasx@outlook.com>
2024-06-23 17:56:46 -0300

    feat: add sign-out

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,12 +4,14 @@ import swagger from '@elysiajs/swagger'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { signOut } from './routes/sign-out'
 
 const app = new Elysia()
   .use(swagger())
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
+  .use(signOut)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
