366759638f49807fbffe6cd7b9e9ce6f1e8831c5
Rafael Freitas <raffreitasx@outlook.com>
2024-06-23 16:47:02 -0300

    feat: add environment and seeds

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,14 +1,19 @@
 import postgres from "postgres";
+import chalk from 'chalk'
 
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 
+import { env } from "../env";
 
-const connection = postgres('postgresql://docker:docker@localhost:5432/pizza_shop', { max: 1 })
+const connection = postgres(env.DATABASE_URL, { max: 1 })
 const db = drizzle(connection)
 
 
 await migrate(db, { migrationsFolder: 'drizzle' })
+
+console.log(chalk.greenBright('Migrations applied successfully!'));
+
 await connection.end()
 
 process.exit()
